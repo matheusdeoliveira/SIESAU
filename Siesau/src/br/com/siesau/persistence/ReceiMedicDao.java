a0473e0df626330dc3cad74b90c51266bd94f557
D.Araujo <D.Araujo@Diogo-Notebook>
2016-05-17 18:29:02 -0300

    Inclusão do Dao para relacionamento de Receita e Medicamento

---
+++ Siesau/src/br/com/siesau/persistence/ReceiMedicDao.java
@@ -0,0 +1,9 @@
+package br.com.siesau.persistence;
+
+import br.com.siesau.entity.ReceiMedic;
+
+public class ReceiMedicDao extends GenericDao<ReceiMedic> {
+	public ReceiMedicDao(ReceiMedic receiMedic){
+		super(receiMedic);
+	}
+}
